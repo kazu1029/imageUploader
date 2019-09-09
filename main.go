e12d324386a580f7c3cc775f4809184a2dc37f12
kazu1029 <imgnt10nxqu05@gmail.com>
2019-09-10 01:02:46 +0900

    add sample

--- main.go
+++ main.go
@@ -2,11 +2,38 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
 )
 
 func uploadFile(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Uploading File")
+	fmt.Println("File Upload Endpoint Hit")
+
+	r.ParseMultipartForm(10 << 20)
+	file, handler, err := r.FormFile("myFile")
+	if err != nil {
+		fmt.Println("Error Retrieving the File")
+		fmt.Println("err")
+		return
+	}
+	defer file.Close()
+	fmt.Printf("Uploaded File: %+v\n", handler.Filename)
+	fmt.Printf("File size: %+v\n", handler.Size)
+	fmt.Printf("MIME Header: %+v\n", handler.Header)
+
+	tempFile, err := ioutil.TempFile("temp-images", "upload-*.png")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer tempFile.Close()
+
+	fileBytes, err := ioutil.ReadAll(file)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	tempFile.Write(fileBytes)
+	fmt.Fprintf(w, "Successfully Uploaded File\n")
 }
 
 func setupRoutes() {
@@ -15,5 +42,5 @@ func setupRoutes() {
 }
 
 func main() {
-	fmt.Println("hello World")
+	setupRoutes()
 }
