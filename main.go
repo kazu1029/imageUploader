3aae2e6e12975427e385a79bcc7836f67d6cd74a
kazu1029 <imgnt10nxqu05@gmail.com>
2019-09-10 16:48:46 +0900

    move upload function to uploader/

--- main.go
+++ main.go
@@ -1,43 +1,13 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
 	"net/http"
+
+	"github.com/kazu1029/imageUploader/uploader"
 )
 
-func uploadFile(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("File Upload Endpoint Hit")
-
-	r.ParseMultipartForm(10 << 20)
-	file, handler, err := r.FormFile("myFile")
-	if err != nil {
-		fmt.Println("Error Retrieving the File")
-		fmt.Println("err")
-		return
-	}
-	defer file.Close()
-	fmt.Printf("Uploaded File: %+v\n", handler.Filename)
-	fmt.Printf("File size: %+v\n", handler.Size)
-	fmt.Printf("MIME Header: %+v\n", handler.Header)
-
-	tempFile, err := ioutil.TempFile("temp-images", "upload-*.png")
-	if err != nil {
-		fmt.Println(err)
-	}
-	defer tempFile.Close()
-
-	fileBytes, err := ioutil.ReadAll(file)
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	tempFile.Write(fileBytes)
-	fmt.Fprintf(w, "Successfully Uploaded File\n")
-}
-
 func setupRoutes() {
-	http.HandleFunc("/upload", uploadFile)
+	http.HandleFunc("/upload", uploader.UploadFile)
 	http.ListenAndServe(":8080", nil)
 }
 
